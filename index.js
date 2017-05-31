deae90f976a2c2ed164d5b3277af430e00d44862
Bennie <brobinson88@gmail.com>
2017-05-31 17:25:19 -0400

    First pass at server version

---
+++ index.js
@@ -0,0 +1,42 @@
+const path    = require('path')
+const express = require('express')
+const app     = express()
+const SpotifyWebApi = require('spotify-web-api-node')
+const config  = require('config')
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: config.get('spotifyClientId'),
+  clientSecret: config.get('spotifyClientSecret')
+})
+
+let access_token = ''
+
+app.use(express.static('public'))
+//app.use(express.static('views'))
+
+app.get('/', function (req, res) {
+  spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body.access_token)
+    access_token = data.body.access_token
+    res.sendFile('views/index.html', {root: __dirname})
+  }, err => {
+    console.error(err)
+  })
+})
+
+app.get('/search', (req, res) => {
+  if (! access_token) {
+    res.send('Please wait for authentication')
+    return
+  }
+
+  spotifyApi.searchAlbums(req.query.q).then(albums => {
+    res.json({albums: albums})
+  }, err => {
+    console.error(err)
+  })
+})
+
+app.listen(process.env.PORT || 8080)
