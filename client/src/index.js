85fc3e1183ca1ebcde84bbe18ce2a9ece8f03f50
Bennie <brobinson88@gmail.com>
2018-03-30 11:35:41 -0400

    Use boostrap package instead of custom file

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "react-bootstrap-table/dist/react-bootstrap-table-all.min.css";
 import "./index.css";
 
